46b8e392d16a1be0f7bf7fca09fd3fa9e736b6c0
kbrousse <kbrousse@2D5.42angouleme.fr>
2025-01-15 15:32:06 +0100

    added constants files for Colors and Names + unmatched route

---
+++ constants/Colors.tsx
@@ -0,0 +1,6 @@
+namespace GLOBAL_COLORS {
+    export enum WELCOME_PAGE {
+        BACKGROUND_COLOR = '#87CEEB',
+    }
+}
+export default GLOBAL_COLORS;
---
+++ constants/Names.tsx
@@ -0,0 +1,5 @@
+enum GLOBAL_NAMES {
+    APP_TITLE = "Swifty-companion",
+}
+
+export default GLOBAL_NAMES;
